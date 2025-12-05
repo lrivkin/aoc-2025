5f61ea7fb2d4da66e0d790e3b8cce87eb291cb62
Leah Rivkin <lrivkin96@gmail.com>
2025-12-04 23:08:16 -0700

    day 5 part 2 test works but my input is wrong

--- day5/test_day5.py
+++ day5/test_day5.py
@@ -8,26 +8,51 @@ def read_input(file: str) -> str:
     with open(p) as f:
         return f.read()
 
-def part_1(file: str):
+def parse_input(file):
     input = read_input(file)
     ranges_in, ingredients_in = input.split("\n\n")
-    fresh_ranges = set()
 
-    ranges: list[tuple[int, int]] = []
+    ranges: list[list[int, int]] = []
 
     for r in ranges_in.splitlines():
         low, high = r.split("-")
-        ranges.append((int(low), int(high)))
+        ranges.append([int(low), int(high)])
+
+    ingredients: list[int] = []
+    for i in ingredients_in.splitlines():
+        ingredients.append(int(i))
 
     ranges = sorted(ranges, key=lambda x: x[0])
+    return ranges, ingredients
+
+def part_1(file: str):
+    ranges, ingredients = parse_input(file)
 
     ans = 0
-    for i in ingredients_in.splitlines():
-        i = int(i)
+    for i in ingredients:
         for low, high in ranges:
             if i >= low and i <= high:
                 ans += 1
                 break
+
+    return ans
+
+def part_2(file: str):
+    ranges, _ = parse_input(file)
+    ans = 0
+    for idx, r in enumerate(ranges):
+        low, high = r
+        ans += high-low+1
+
+        j = idx+1
+        while j < len(ranges):
+            if ranges[j][0] > high:
+                break
+            ranges[j][0] = high+1
+            ranges[j][1] = max(ranges[j][1], high+1)
+            j+= 1
+
+
 
     return ans
 
@@ -36,5 +61,8 @@ class TestDay5(TestCase):
         self.assertEqual(3, part_1("test.txt"))
 
     def test_part_1_real(self):
-        ans = part_1("input.txt")
-        self.assertEqual(-1, ans)
+        print(part_1("input.txt"))
+
+    def test_part_2(self):
+        self.assertEqual(14, part_2("test.txt"))
+        print(part_2("input.txt"))
