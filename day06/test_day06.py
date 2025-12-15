07444a5ae574633ae2ab09258cc4c098b6a26ceb
Leah Rivkin <lrivkin96@gmail.com>
2025-12-14 18:44:34 -0700

    day 6 part 2 works for example

--- day06/test_day06.py
+++ day06/test_day06.py
@@ -32,11 +32,68 @@ def part_1(filename):
     return overall_ans
 
 
+def part_2(filename):
+    p = Path(__file__).with_name(filename)
+    with open(p) as f:
+        input_raw = f.read()
+        lines = input_raw.splitlines()
+
+        num_cols = len(lines[0])
+        num_rows = len(lines)
+
+        col = num_cols-1
+
+        nums = []
+        numbers = []
+        operations = []
+        while col >= 0:
+            num = ""
+            for row in range(num_rows-1):
+                if lines[row][col] != " ":
+                    num += lines[row][col]
+
+            if num.strip() == "" or col == 0:
+                if col == 0:
+                    col = -1
+                operations.append(lines[row+1][col+1])
+                numbers.append(nums)
+                nums = []
+
+            else:
+                nums.append(int(num))
+
+            col -= 1
+
+        overall_ans = 0
+        for idx, operation in enumerate(operations):
+            if operation == "*":
+
+                ans = 1
+                for n in numbers[idx]:
+                    ans *= n
+
+            elif operation == "+":
+                ans = 0
+                for n in numbers[idx]:
+                    ans += n
+            else:
+                pass
+
+            overall_ans += ans
+
+        print(overall_ans)
+
+
 
 class Test(TestCase):
     def test_part1(self):
         part_1("test.txt")
 
+    def test_part1_real(self):
+        part_1("input.txt")
 
     def test_part2(self):
-        part_1("input.txt")
+        part_2("test.txt")
+
+    def test_part2_real(self):
+        part_2("input.txt")
