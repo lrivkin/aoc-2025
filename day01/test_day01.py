cd56772b6c96ee83001ac4f646b851f82b1d583e
Leah Rivkin <lrivkin96@gmail.com>
2025-12-21 16:28:07 -0500

    day 1 part 2 WIP but incorrect

--- day01/test_day01.py
+++ day01/test_day01.py
@@ -10,20 +10,42 @@ def read_input(file: str):
 def part_2(file):
     input = read_input(file).splitlines()
 
-    start_pos = 50
+    num_zeros = 0
+
+    position = 50
     for line in input:
-        if line[0] == 'L':
-            pass
-        elif[0] == 'R':
-            pass
+        line = line.strip()
+        dir = line[0]
+        num = int(line[1:])
+        if dir == 'L':
+            position -= num
+        elif dir == 'R':
+            position += num
         else:
             raise ValueError()
+        
+        if 0 <= position < 99:
+            continue
 
-    return -1
+        if position < 0:
+            while position < 0:
+                position += 100
+                num_zeros += 1
+
+        elif position >= 100:
+            while position >= 100:
+                position -= 100
+                num_zeros += 1
+
+    return num_zeros
 
 class Test(TestCase):
     def test_part1(self):
-        self.assertEqual(6, part_2("test.txt"))
+        pass
+
+    def test_part2_real(self):
+        # 6536: not right? 
+        print(part_2("input.txt"))
 
     def test_part2(self):
-        pass
+        self.assertEqual(6, part_2("test.txt"))
