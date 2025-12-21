d4261eb7d79e1e0f2e6f99282efbe45f0bc9b0dd
Leah Rivkin <lrivkin96@gmail.com>
2025-12-20 19:36:19 -0500

    day 3 part 1 finished

--- day03/test_day03.py
+++ day03/test_day03.py
@@ -2,15 +2,11 @@ from unittest import TestCase
 from pathlib import Path
 
 def joltage(input: str) -> int:
-    max_val = input[0]
-    second_max = input[len(input)-1]
-    for i in range(1, len(input)-1):
-        if input[i] > max_val:
-            max_val = input[i]
-            second_max = input[i+1]
-        elif input[i] > second_max:
-            second_max = input[i]
-    return int(max_val + second_max)
+    biggest = max(input[:len(input)-1])
+    biggest_idx = input.index(biggest)
+
+    next_biggest = max(input[biggest_idx+1:])
+    return int(biggest+next_biggest)
 
 def part_1(file):
     p = Path(__file__).with_name(file)
@@ -41,8 +37,7 @@ class Test(TestCase):
         self.assertEqual(357, part_1("test.txt"))
 
     def test_run_part1(self):
-        # Result: 17324
-        # too low. Curiously, it's the right answer for someone else; you might be logged in to the wrong account or just unlucky
+        # Result: 17332
         print(part_1("input.txt"))
 
     def test_part2(self):
